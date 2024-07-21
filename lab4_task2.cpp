06a21ee924758667403998d9311387b65b2e57b9
Darya Kuznetsova <Dashar1996@gmail.com>
2024-07-21 08:56:08 +0300

    Add files via upload

---
+++ lab4_task2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+bool input(int& a, int& b) {
+	if (!(cin >> a >> b)) {
+		cin.clear();
+		return false;
+	}
+	return true;
+}
+int main() {
+	system("chcp 1251");
+	int numb;
+	int a, b;
+	cout << "Введите два целых числа ";
+	if (!input(a, b)) {
+		cerr << "Некорректные данные" << endl;
+		return 1;
+	}
+	int s = a + b;
+	cout << "Сумма " << s << endl;
+	return 0;
+}
