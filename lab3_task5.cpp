a6a862397be7fa730ed513c5a042ee6e22450b40
Darya Kuznetsova <Dashar1996@gmail.com>
2024-07-21 08:55:46 +0300

    Add files via upload

---
+++ lab3_task5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string fromDecToBin(int num) {
+	if (num == 0)
+		return "";
+	string binary = fromDecToBin(num / 2);
+	return binary + to_string(num % 2);
+
+}
+int main() {
+	system("chcp 1251");
+	int num;
+	cout << "Введите число ";
+	cin >> num;
+	if (num == 0) {
+		cout << "Двоичный результат 0" << endl;
+	}
+	else {
+		cout << "Двоичный результат " << fromDecToBin(num) << endl;
+	}
+}
