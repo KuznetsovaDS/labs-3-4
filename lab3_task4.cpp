8f1fc3ecd22ee3188a6fb9cbc39044c512503fbf
Darya Kuznetsova <Dashar1996@gmail.com>
2024-07-21 08:55:37 +0300

    Add files via upload

---
+++ lab3_task4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sumLine(int n) {
+    if (n == 1) {
+        return 5;
+    }
+    return sumLine(n - 1) + 5 * n;
+}
+int main() {
+    system("chcp 1251");
+    int n;
+    cout << "Введите значение n ";
+    cin >> n;
+    if (n > 0) {
+        int result = sumLine(n);
+        cout << "Сумма ряда n = " << n << " равна " << result << endl;
+    }
+    else {
+        cout << " некорректное значение n" << endl;
+    }
+
+}
